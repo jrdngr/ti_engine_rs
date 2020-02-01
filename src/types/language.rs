404c45531715756ccf991481e4bf604e86eabade
Jordan Grace <jordan.t.grace@gmail.com>
2020-01-31 20:16:00 -0500

    Made some utilities

---
+++ src/types/language.rs
@@ -0,0 +1,55 @@
+use std::str::FromStr;
+
+#[allow(non_camel_case_types)]
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum Language {
+    en_US,
+    fe_FR,
+    es_ES,
+    de_DE,
+    ja_JP,
+}
+
+impl FromStr for Language {
+    type Err = anyhow::Error;
+
+    fn from_str(string: &str) -> Result<Self, Self::Err> {
+        match string {
+            "en_US" => Ok(Language::en_US),
+            "fe_FR" => Ok(Language::fe_FR),
+            "es_ES" => Ok(Language::es_ES),
+            "de_DE" => Ok(Language::de_DE),
+            "ja_JP" => Ok(Language::ja_JP),
+            _ => anyhow::bail!("Invalid input: {}", string),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn language_as_int() {
+        assert_eq!(Language::en_US as u32, 0);
+        assert_eq!(Language::fe_FR as u32, 1);
+        assert_eq!(Language::es_ES as u32, 2);
+        assert_eq!(Language::de_DE as u32, 3);
+        assert_eq!(Language::ja_JP as u32, 4);
+    }
+
+    #[test]
+    fn language_as_string_valid() {
+        assert_eq!(Language::from_str("en_US").unwrap(), Language::en_US);
+        assert_eq!(Language::from_str("fe_FR").unwrap(), Language::fe_FR);
+        assert_eq!(Language::from_str("es_ES").unwrap(), Language::es_ES);
+        assert_eq!(Language::from_str("de_DE").unwrap(), Language::de_DE);
+        assert_eq!(Language::from_str("ja_JP").unwrap(), Language::ja_JP);
+    }
+
+    #[test]
+    fn language_as_string_invalid() {
+        assert!(Language::from_str("oh noes").is_err());
+        assert!(Language::from_str("kl_KL").is_err());
+    }
+}
---
+++ src/utilities.rs
@@ -0,0 +1,50 @@
+use std::f64::consts::PI;
+
+pub fn to_radians(degrees: f64) -> f64 {
+    (degrees * PI) / 180.0
+}
+
+pub fn to_degrees(radians: f64) -> f64 {
+    (radians * 180.0) / PI
+}
+
+pub fn string_to_int(string: &str) -> i32 {
+    match string.parse::<i32>() {
+        Ok(num) => num,
+        Err(_)  => 0,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_to_radians() {
+        assert_eq!(to_radians(0.0), 0.0);
+        assert_eq!(to_radians(14.323945045471191), 0.2500000029182011);
+        assert_eq!(to_radians(28.647890090942383), 0.5000000058364023);
+        assert_eq!(to_radians(42.971836090087891), 0.7500000253993603);
+        assert_eq!(to_radians(57.2957801818847661), 1.0000000116728045);
+    }
+
+    #[test]
+    fn test_to_degrees() {
+        assert_eq!(to_degrees(0.0), 0.0);
+        assert_eq!(to_degrees(0.2500000029182011), 14.323945045471188);
+        assert_eq!(to_degrees(0.5000000058364023), 28.647890090942376);
+        assert_eq!(to_degrees(0.7500000253993603), 42.971836090087891);
+        assert_eq!(to_degrees(1.0), 57.29577951308232);    
+    }
+
+    #[test]
+    fn test_string_to_int() {
+        let one = string_to_int("1");
+        let two = string_to_int("2");
+        let three = string_to_int("3");
+    
+        assert_eq!(one, 1);
+        assert_eq!(two, 2);
+        assert_eq!(three, 3);
+    }
+}
